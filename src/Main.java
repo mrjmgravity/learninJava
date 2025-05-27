86acdf33aeecc4704d5ac97be9504d179cdb1ea2
mrjmgravity <sevenfold0198@gmail.com>
2025-05-27 18:43:16 +0200

    general
    general

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Ahoj gf");
+        System.out.println("Ahoj");
+        System.out.println("gf");
+        System.out.println();
+    }
+}
+
