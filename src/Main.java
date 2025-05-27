73d9cccf4e50f878b1c439a5fd5b7722b085d689
mrjmgravity <sevenfold0198@gmail.com>
2025-05-27 19:33:35 +0200

    Scanner
    Scanner

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Ahoj gf");
-        System.out.println("Ahoj");
-        System.out.println("gf");
-        System.out.println();
+        Scanner myScanner = new Scanner(System.in);
+
     }
 }
 
