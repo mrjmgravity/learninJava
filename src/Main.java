5e353147d0d7721980ba0aee273c0c25d062bf70
mrjmgravity <sevenfold0198@gmail.com>
2025-06-03 18:27:14 +0200

    For and foreach

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,20 @@ public class Main {
         String meno = myScanner.nextLine();
         System.out.println( pozdrav + meno);
 
+
+        for(int i = 0; i<10; i++){
+            System.out.println(i*5);
+
+        }
+        String[] mena = new String[] {"Jan", "Peter", "Katka","Jana"};
+
+        for(String jmeno: mena ) {
+            System.out.println(jmeno);
+
+        }
+
+        }
+
     }
-}
+
 
