dbd7a2c84ef2252fcfa1bdde0bd1afb434fcce57
mrjmgravity <sevenfold0198@gmail.com>
2025-06-03 18:06:39 +0200

    first
    first

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
 
+        String pozdrav = "Ahoj moje meno je: ";
         Scanner myScanner = new Scanner(System.in);
+        System.out.println("Zadaj svoje meno: ");
+        String meno = myScanner.nextLine();
+        System.out.println( pozdrav + meno);
 
     }
 }
